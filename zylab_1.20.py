3d0223040020f59caf8e7deb91adc83675d47ef4
aexcell <70186926+aexcell@users.noreply.github.com>
2020-09-14 22:58:55 -0500

    Update zylab_1.20.py
    1.20 CIS 2348 LAB: Warm up: Basic output with variables

--- zylab_1.20.py
+++ zylab_1.20.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Alexandra Excell PSID: 1595971
 
 user_num1 = int(input('Enter integer:\n'))
 num_squared = user_num1**2
