fc89e77f81fe423f41d7bc69848e96a6685b7044
aexcell <70186926+aexcell@users.noreply.github.com>
2020-09-14 22:48:26 -0500

    Add files via upload
    1.20 CIS 2348 LAB: Warm up: Basic output with variables

---
+++ 1.20.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+user_num1 = int(input('Enter integer:\n'))
+num_squared = user_num1**2
+num_cubed = user_num1**3
+
+print('You entered:', user_num1)
+print(user_num1, 'squared is', num_squared)
+print('And', user_num1, 'cubed is' , num_cubed, '!!')
+
+user_num2= int(input('Enter another integer:\n'))
+add_output = user_num1 + user_num2
+prod_output = user_num1 * user_num2
+
+print(user_num1,'+',user_num2, 'is', add_output)
+print(user_num1,'*', user_num2, 'is', prod_output)
+
+
+
