7cb049e2a8004a227c886139dfa111d63b83e3de
aexcell <70186926+aexcell@users.noreply.github.com>
2020-11-27 21:55:38 -0600

    Delete zylab_11.18.py

--- zylab_11.18.py
+++
@@ -1,9 +0,0 @@
-
-numList = []
-numbers = [int(num) for num in input().split()]
-
-sorted_num = sorted(numbers)
-for num in sorted_num:
-    if num >= 0:
-        numList.append(num)
-print(*numList, end=" ")
